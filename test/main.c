4e56f43c6984940a9d94b78ba1ce5c04a872fdd2
smallfish <461146760@qq.com>
2024-05-02 10:20:49 +0800

    add test

--- test/main.c
+++ test/main.c
@@ -8,14 +8,20 @@
 ***********************************************************************/
 
 #include "logs.h"
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+#define COLORFUL_LOG(fmt, ...) \
+    do { \
+        LOGI(fmt , ##__VA_ARGS__); \
+        LOGW(fmt , ##__VA_ARGS__); \
+        LOGE(fmt , ##__VA_ARGS__); \
+        LOGD(fmt , ##__VA_ARGS__); \
+        LOGV(fmt , ##__VA_ARGS__); \
+    } while (0)
+
+int main(int argc, char** argv)
 {
-    LOGI("Hello, world!");
-    LOGW("Hello, world!");
-    LOGE("Hello, world!");
-    LOGD("Hello, world!");
-    LOGV("Hello, world!");
+    COLORFUL_LOG("%s, %S", getenv("LPAC_APDU"), getenv("LPAC_APDU"));
 
     return 0;
 }
